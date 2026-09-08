20dee5659a449a52cf3d32e6d0e833515616b916
GVonFlue <gvonflue@gmail.com>
2026-09-08 17:36:31 -0500

    Delete app/areas/page.tsx

--- app/areas/page.tsx
+++
@@ -1,8 +0,0 @@
-import { pages } from "@/lib/content";
-import { metadataFor, renderPage } from "@/lib/page-factory";
-
-export const metadata = metadataFor(pages.areas);
-
-export default function Page() {
-  return renderPage(pages.areas);
-}
